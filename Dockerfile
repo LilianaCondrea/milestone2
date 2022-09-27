619dde00187a079e8a746742c60d39fcef1e73f1
Liliana Condrea <liliana.condrea@internal.ebs.md>
2022-09-27 16:24:21 +0300

    postgres_setup

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,11 @@ ENV PYTHONUNBUFFERED=1
 WORKDIR /apps
 
 # Installing python dependencies
-COPY . /apps
+
 RUN python -m pip install --upgrade pip
 RUN python -m pip install psycopg2
 RUN python -m pip install -r requirements.txt
-
+COPY . /apps
 # Exposing Ports
 EXPOSE 5432
 EXPOSE 8080
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -85,18 +85,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.environ.get('POSTGRES_NAME'),
-    #     'USER': os.environ.get('POSTGRES_USER'),
-    #     'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-    #     'HOST': 'db',
-    #     'PORT': 5432,
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
