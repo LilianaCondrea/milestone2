dfea9d2a8e2dc0a317b986396912a4f73d5cd64f
Liliana Condrea <liliana.condrea@internal.ebs.md>
2022-10-06 13:47:33 +0300

    custom_command_refactor

--- apps/common/management/commands/generate_tasks.py
+++ apps/common/management/commands/generate_tasks.py
@@ -4,26 +4,37 @@ from django.core.management import BaseCommand
 from faker import Faker
 
 from ....tasks.models import Task
+from ....timelogs.models import TimeLog
 from ....users.models import User
 
-faker = Faker()
+
 
 
 class Command(BaseCommand):
     help = 'Generate tasks'
 
-    def add_arguments(self, parser):
-        parser.add_argument('--amount', type=int, help='amount of tasks to generate')
+    # def add_arguments(self, parser):
+    #     parser.add_argument('--amount', type=int, help='amount of tasks to generate')
 
     def handle(self, *args, **options):
-        count = 0
-        for _ in range(250):
+        fake = Faker()
+        owner = User.objects.get(pk=2)
+        for i in range(25000):
             task = Task.objects.create(
-                title=faker.unique.first_name(),
-                description=faker.name(),
-                status=faker.boolean(),
-                owner=random.choice(User.objects.all())
+                title=fake.unique.name(),
+                description=fake.word(),
+                status=fake.boolean(),
+                owner=owner
             )
-            count += 1
-            print('Task ' + str(task.title) + ' generated:' + str(count))
-            print('were generated ' + str(count) + ' tasks')
+            task.save()
+
+            timelog = TimeLog.objects.create(
+                    start_timer=fake.past_datetime(),
+                    end_timer=fake.future_datetime(),
+                    task=task,
+                    owner=owner
+            )
+            timelog.save()
+            # count += 1
+            # print('Task ' + str(task.title) + ' generated:' + str(count))
+            # print('were generated ' + str(count) + ' tasks')
